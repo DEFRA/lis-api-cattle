29cdce3e869c7063e16bba91c33e7b7e2335e3a8
Gary Woodfine <gary.woodfine@threenine.co.uk>
2026-07-29 14:50:05 +0100

    Feature/lreg 41 fix namespace (#3)
    * "Add Defra.Lis.Postgres package and update Database project dependencies"
    
    * "Add project reference to Database and configure PostgreSQL integration in Api project"
    
    * "Add LocalStack setup and update PostgreSQL configuration for lis_cattle integration"
    
    * "Add Dockerfile for building and running the API, update AWS configuration, and bump Defra.Lis package versions to 0.0.3"
    
    * Add Liquibase configuration with changelog, properties, and schema setup for PostgreSQL integration

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -2,7 +2,9 @@ using Defra.Database.Postgres;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
+// Add services to the container.
+// Learn more about configuring
+//
  builder.Services.AddPostgresDatabase(builder.Configuration);
 
 var app = builder.Build();
